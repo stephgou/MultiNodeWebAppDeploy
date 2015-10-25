9426e35d8060f32c9973b81bf4bafbee2c6c3713
stephgou <stephgou@microsoft.com>
2015-10-25 14:00:20 +0100

    siteNameProxyappsettings

--- deploy.cmd
+++ deploy.cmd
@@ -1,9 +1,9 @@
 @echo off
 
-IF "%SITE_FLAVOR%" == "nodesrv" (
+IF "%SITE_CONFIG%" == "nodesrv" (
   deploy.nodesrv.cmd
 ) ELSE (
-  IF "%SITE_FLAVOR%" == "nodeproxy" (
+  IF "%SITE_CONFIG%" == "nodeproxy" (
     deploy.nodeproxy.cmd
   ) ELSE (
     echo You have to set SITE_FLAVOR setting to either "nodesrv" or "nodeproxy"
